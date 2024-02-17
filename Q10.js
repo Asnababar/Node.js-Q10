6124aef86e2998d62ddd7fc6f99176787ea9cb6f
Asnababar <147834277+Asnababar@users.noreply.github.com>
2024-02-17 21:34:20 +0500

    Node.js Q10

---
+++ Q10.js
@@ -0,0 +1,13 @@
+"use strict";
+// Addition comments: choose two of the program your written, and add atleast one comment beacause // your program are to simple at this Point, just add your and the current description what the program decodedText span enter intesect switch.
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 10;
+let b = 20;
+//Addition
+console.log(`${a} + ${b} = ${a + b}`);
+//Substactiom
+console.log(`${a} - ${b} = ${a - b}`);
+//Multiplication
+console.log(`${a} * ${b} = ${a * b}`);
+//Division
+console.log(`${a} / ${b} = ${a / b}`);
---
+++ Q10.ts
@@ -0,0 +1,20 @@
+// Addition comments: choose two of the program your written, and add atleast one comment beacause // your program are to simple at this Point, just add your and the current description what the program decodedText span enter intesect switch.
+
+let a: number = 10
+let b: number = 20
+
+//Addition
+
+console.log(`${a} + ${b} = ${a + b}`)
+
+//Substactiom
+
+console.log(`${a} - ${b} = ${a - b}`)
+
+//Multiplication
+
+console.log(`${a} * ${b} = ${a * b}`)
+
+//Division
+
+console.log(`${a} / ${b} = ${a / b}`)
